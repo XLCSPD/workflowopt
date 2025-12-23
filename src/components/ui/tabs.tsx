0cbcad3d3c947ef119f627cfa5e212b06e402331
XLCSPD <asasore@defoxx.com>
2025-12-22 21:48:32 -0500

    Fix Tabs component to enforce flex layout with inline styles

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -5,7 +5,19 @@ import * as TabsPrimitive from "@radix-ui/react-tabs"
 
 import { cn } from "@/lib/utils"
 
-const Tabs = TabsPrimitive.Root
+// Custom Tabs wrapper to properly support flex layout
+const Tabs = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
+>(({ className, style, ...props }, ref) => (
+  <TabsPrimitive.Root
+    ref={ref}
+    className={cn("", className)}
+    style={{ display: 'flex', flexDirection: 'column', ...style }}
+    {...props}
+  />
+))
+Tabs.displayName = TabsPrimitive.Root.displayName
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
